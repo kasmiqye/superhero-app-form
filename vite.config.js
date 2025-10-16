44788a8a996149b466972bc1137898ce30a97684
kasmiqye <poamrb@gmail.com>
2025-10-16 14:08:16 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-/*export default defineConfig({
+export default defineConfig({
   plugins: [react()],
-}) */
-export default {
   base: '/superhero-app-form/',
-};
+})
